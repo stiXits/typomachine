190c274d2eb715cc63c1a9a97112cd8818737d2b
stix-desktop <stix@phcn.de>
2014-03-16 20:07:47 +0100

    added mirroupdate while first vbox setup

--- bootstrap/setup_all.sh
+++ bootstrap/setup_all.sh
@@ -1,11 +1,13 @@
 cd /vagrant/bootstrap
 
+./setup_mirrors.sh
 ./setup_installation.sh
 ./setup_apache.sh
 ./setup_php.sh
 ./setup_mysql.sh
 
-./setup_composer.sh
-./setup_flow.sh
+echo "### cleaning pacman cache ###"
+sudo pacman -Scc --noconfirm
+rm  -rf /var/cache/pacman/pkg/* 
 
 cd /vagrant
